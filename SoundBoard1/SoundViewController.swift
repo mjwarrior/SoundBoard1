242632e89c89d03343ec51f47fcf3cb310c2d113
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-04-03 16:06:51 -0500

    I just finished lecture 88 section 8 proceeding to lecture 89 now we have a navigation controller and we also have buttons and a textfield and a plus button and I have put the textfield and buttons  into code and I have a new view controller called Sound view controller

---
+++ SoundBoard1/SoundViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundViewController.swift
+//  SoundBoard1
+//
+//  Created by Marc Streeter on 4/3/17.
+//  Copyright © 2017 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameTextField: UITextField!
+   
+    @IBOutlet weak var recordButton: UIButton!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+ 
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+   
+    
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
